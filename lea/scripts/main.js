cf2c26ea078a4efca3a0f62b6e7ceb841705830e
leaddn <leadieudonat@gmail.com>
2021-03-28 18:57:29 +0200

    add javascript

---
+++ lea/scripts/main.js
@@ -0,0 +1,19 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Veuillez saisir votre nom.');
+    localStorage.setItem('nom', myName);
+    myHeading.textContent = `What are you doing here ${myName} ?`;
+  }
+
+  if (!localStorage.getItem('nom')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('nom');
+    myHeading.textContent = `What are you doing here ${storedName} ?`;
+  }
+
+  myButton.addEventListener('click', function() {
+    setUserName();
+  });
