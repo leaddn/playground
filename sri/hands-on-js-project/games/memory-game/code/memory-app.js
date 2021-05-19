bb6374022219e8c09336334b8596d09c12c06ed6
srilakshmibalard <50013043+srilakshmibalard@users.noreply.github.com>
2021-05-20 00:50:40 +0200

    add some comment - explanation
    hope the code is readable and the comments are useful

--- sri/hands-on-js-project/games/memory-game/code/memory-app.js
+++ sri/hands-on-js-project/games/memory-game/code/memory-app.js
@@ -9,7 +9,7 @@ let firstCard, secondCard;
 let lock= false;
 
 cards.forEach(card => {
-  card.addEventListener('click', cardReturned);
+  card.addEventListener('click', cardReturned); /* écoute de l'évènement*/
 })
 
 /*===============================================================================
@@ -38,6 +38,14 @@ function cardReturned(){
   This function, correspondance(), let the card returned.
   That means each time we found the right combination, 
   the cards do not flip back
+  removeEventListener() : supprime l'évènement précédemment écouté
+  ainsi l'évènement à supprimer est identifié puisque cet évènement
+  a été déclenché dans la function carReturned 
+  dans notre cas 
+  firstCard.removeEventListener('click',cardReturned)
+  firstCard est l'évènement cible
+'click' est le type de l'évènement pour lequel on souhaite supprimer l'écouteur d'évenement
+cardReturned est la fonction du gestionnaire d'évènement à supprimer
 ================================================================================*/
 function correspondance() {
   if(firstCard.getAttribute('data-attr') === secondCard.getAttribute('data-attr')){
@@ -46,6 +54,11 @@ function correspondance() {
     // alert(`Great!, you find the right combination`);
   }
   else{
+    /*
+    Si les deux cartes retournées ne sont pas identiques
+    alors leur face avec l'image est retourné 
+    après un lapse de temps de 1s
+    */
     lock = true;
     setTimeout(() => {
       firstCard.childNodes[1].classList.remove('active');
@@ -57,11 +70,11 @@ function correspondance() {
   
 }
 
-/*===============================================================================
-                          function randomSelectedCard()
-  This function shuffle the cards.
-  Each time we refresh the page, each card position change
-================================================================================*/
+// /*===============================================================================
+//                           function randomSelectedCard()
+//   This function shuffle the cards.
+//   Each time we refresh the page, each card position change
+// ================================================================================*/
 function randomSelectedCard(){
   cards.forEach(card =>{
     let randomPos = Math.floor(Math.random() * 12);
@@ -70,4 +83,7 @@ function randomSelectedCard(){
 }
 
 randomSelectedCard();
+
+
+
 
